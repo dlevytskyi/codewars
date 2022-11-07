4013c206bbb124a68c8761f01dcbd78b86374e14
Dmytro Levytskyi <dimalevytskyi@gmail.com>
2022-11-07 11:21:41 +0100

    added Does my number look big in this solution

---
+++ tasks/Does my number look big in this?/index.ts
@@ -0,0 +1,22 @@
+import { expect } from 'chai';
+
+export function narcissistic(value: number): boolean {
+  let array: Array<number> = value
+    .toString()
+    .split('')
+    .map((el) => Number(el));
+  let num: number = 0;
+  for (let i = 0; i < array.length; i++) {
+    num += Math.pow(array[i], array.length);
+  }
+
+  return num === value;
+}
+
+describe('Does my number look big in this? test', () => {
+  it('Basic test should work', () => {
+    expect(narcissistic(7)).to.equal(true, '7 is narcissistic');
+    expect(narcissistic(153)).to.equal(true, '153 is narcissistic');
+    expect(narcissistic(1634)).to.equal(true, '1634 is narcissistic');
+  });
+});
